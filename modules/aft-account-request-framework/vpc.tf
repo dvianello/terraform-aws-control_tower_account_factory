3d5559864465959404bd8f948f52c6042edc469c
Morgan Rowse <morgan@lsd.co.za>
2022-10-18 15:05:48 +0200

    feat: add opt-in feature flag `aft_feature_disable_private_networking` to disable private networking infrastructure
    - run lambdas in public subnets
    - run codebuild in public subnets
    - run codestar in public subnets
    - remove private natgw and subnets
    - sparse formatting

--- modules/aft-account-request-framework/vpc.tf
+++ modules/aft-account-request-framework/vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "aft_vpc" {
   cidr_block           = var.aft_vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
+  tags                 = {
     Name = "aft-management-vpc"
   }
 }
@@ -15,19 +15,21 @@ resource "aws_vpc" "aft_vpc" {
 #########################################
 
 resource "aws_subnet" "aft_vpc_private_subnet_01" {
+  count             = var.aft_feature_disable_private_networking ? 0 : 1
   vpc_id            = aws_vpc.aft_vpc.id
   cidr_block        = var.aft_vpc_private_subnet_01_cidr
   availability_zone = element(data.aws_availability_zones.available.names, 0)
-  tags = {
+  tags              = {
     Name = "aft-vpc-private-subnet-01"
   }
 }
 
 resource "aws_subnet" "aft_vpc_private_subnet_02" {
+  count             = var.aft_feature_disable_private_networking ? 0 : 1
   vpc_id            = aws_vpc.aft_vpc.id
   cidr_block        = var.aft_vpc_private_subnet_02_cidr
   availability_zone = element(data.aws_availability_zones.available.names, 1)
-  tags = {
+  tags              = {
     Name = "aft-vpc-private-subnet-02"
   }
 }
@@ -36,7 +38,7 @@ resource "aws_subnet" "aft_vpc_public_subnet_01" {
   vpc_id            = aws_vpc.aft_vpc.id
   cidr_block        = var.aft_vpc_public_subnet_01_cidr
   availability_zone = element(data.aws_availability_zones.available.names, 0)
-  tags = {
+  tags              = {
     Name = "aft-vpc-public-subnet-01"
   }
 }
@@ -45,7 +47,7 @@ resource "aws_subnet" "aft_vpc_public_subnet_02" {
   vpc_id            = aws_vpc.aft_vpc.id
   cidr_block        = var.aft_vpc_public_subnet_02_cidr
   availability_zone = element(data.aws_availability_zones.available.names, 1)
-  tags = {
+  tags              = {
     Name = "aft-vpc-public-subnet-02"
   }
 }
@@ -56,10 +58,11 @@ resource "aws_subnet" "aft_vpc_public_subnet_02" {
 #########################################
 
 resource "aws_route_table" "aft_vpc_private_subnet_01" {
+  count             = var.aft_feature_disable_private_networking ? 0 : 1
   vpc_id = aws_vpc.aft_vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.aft-vpc-natgw-01.id
+    nat_gateway_id = aws_nat_gateway.aft-vpc-natgw-01[0].id
   }
   tags = {
     Name = "aft-vpc-private-subnet-01"
@@ -67,10 +70,11 @@ resource "aws_route_table" "aft_vpc_private_subnet_01" {
 }
 
 resource "aws_route_table" "aft_vpc_private_subnet_02" {
+  count             = var.aft_feature_disable_private_networking ? 0 : 1
   vpc_id = aws_vpc.aft_vpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.aft-vpc-natgw-02.id
+    nat_gateway_id = aws_nat_gateway.aft-vpc-natgw-02[0].id
   }
   tags = {
     Name = "aft-vpc-private-subnet-02"
@@ -89,13 +93,15 @@ resource "aws_route_table" "aft_vpc_public_subnet_01" {
 }
 
 resource "aws_route_table_association" "aft_vpc_private_subnet_01" {
-  subnet_id      = aws_subnet.aft_vpc_private_subnet_01.id
-  route_table_id = aws_route_table.aft_vpc_private_subnet_01.id
+  count             = var.aft_feature_disable_private_networking ? 0 : 1
+  subnet_id      = aws_subnet.aft_vpc_private_subnet_01[0].id
+  route_table_id = aws_route_table.aft_vpc_private_subnet_01[0].id
 }
 
 resource "aws_route_table_association" "aft_vpc_private_subnet_02" {
-  subnet_id      = aws_subnet.aft_vpc_private_subnet_02.id
-  route_table_id = aws_route_table.aft_vpc_private_subnet_02.id
+  count             = var.aft_feature_disable_private_networking ? 0 : 1
+  subnet_id      = aws_subnet.aft_vpc_private_subnet_02[0].id
+  route_table_id = aws_route_table.aft_vpc_private_subnet_02[0].id
 }
 
 resource "aws_route_table_association" "aft_vpc_public_subnet_01" {
@@ -168,17 +174,20 @@ resource "aws_internet_gateway" "aft-vpc-igw" {
 }
 
 resource "aws_eip" "aft-vpc-natgw-01" {
-  vpc = true
+  count = var.aft_feature_disable_private_networking ? 0 : 1
+  vpc   = true
 }
 
 resource "aws_eip" "aft-vpc-natgw-02" {
-  vpc = true
+  count = var.aft_feature_disable_private_networking ? 0 : 1
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "aft-vpc-natgw-01" {
+  count      = var.aft_feature_disable_private_networking ? 0 : 1
   depends_on = [aws_internet_gateway.aft-vpc-igw]
 
-  allocation_id = aws_eip.aft-vpc-natgw-01.id
+  allocation_id = aws_eip.aft-vpc-natgw-01[0].id
   subnet_id     = aws_subnet.aft_vpc_public_subnet_01.id
 
   tags = {
@@ -188,9 +197,10 @@ resource "aws_nat_gateway" "aft-vpc-natgw-01" {
 }
 
 resource "aws_nat_gateway" "aft-vpc-natgw-02" {
+  count      = var.aft_feature_disable_private_networking ? 0 : 1
   depends_on = [aws_internet_gateway.aft-vpc-igw]
 
-  allocation_id = aws_eip.aft-vpc-natgw-02.id
+  allocation_id = aws_eip.aft-vpc-natgw-02[0].id
   subnet_id     = aws_subnet.aft_vpc_public_subnet_02.id
 
   tags = {
@@ -209,7 +219,10 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.aft_vpc.id
   vpc_endpoint_type = "Gateway"
   service_name      = "com.amazonaws.${data.aws_region.aft-management.name}.s3"
-  route_table_ids   = [aws_route_table.aft_vpc_private_subnet_01.id, aws_route_table.aft_vpc_private_subnet_02.id, aws_route_table.aft_vpc_public_subnet_01.id]
+  route_table_ids   = [
+    aws_route_table.aft_vpc_private_subnet_01[0].id, aws_route_table.aft_vpc_private_subnet_02[0].id,
+    aws_route_table.aft_vpc_public_subnet_01.id
+  ]
 }
 
 resource "aws_vpc_endpoint" "dynamodb" {
@@ -218,7 +231,10 @@ resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id            = aws_vpc.aft_vpc.id
   vpc_endpoint_type = "Gateway"
   service_name      = "com.amazonaws.${data.aws_region.aft-management.name}.dynamodb"
-  route_table_ids   = [aws_route_table.aft_vpc_private_subnet_01.id, aws_route_table.aft_vpc_private_subnet_02.id, aws_route_table.aft_vpc_public_subnet_01.id]
+  route_table_ids   = [
+    aws_route_table.aft_vpc_private_subnet_01[0].id, aws_route_table.aft_vpc_private_subnet_02[0].id,
+    aws_route_table.aft_vpc_public_subnet_01.id
+  ]
 }
 
 #########################################
@@ -228,10 +244,10 @@ resource "aws_vpc_endpoint" "dynamodb" {
 resource "aws_vpc_endpoint" "codebuild" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.codebuild[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.codebuild[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.codebuild[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.codebuild[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -242,10 +258,10 @@ resource "aws_vpc_endpoint" "codebuild" {
 resource "aws_vpc_endpoint" "codecommit" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.codecommit[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.codecommit[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.codecommit[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.codecommit[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -256,10 +272,10 @@ resource "aws_vpc_endpoint" "codecommit" {
 resource "aws_vpc_endpoint" "git-codecommit" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.git-codecommit[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.git-codecommit[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.git-codecommit[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.git-codecommit[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -270,10 +286,10 @@ resource "aws_vpc_endpoint" "git-codecommit" {
 resource "aws_vpc_endpoint" "codepipeline" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.codepipeline[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.codepipeline[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.codepipeline[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.codepipeline[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -284,10 +300,10 @@ resource "aws_vpc_endpoint" "codepipeline" {
 resource "aws_vpc_endpoint" "servicecatalog" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.servicecatalog[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.servicecatalog[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.servicecatalog[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.servicecatalog[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -298,10 +314,10 @@ resource "aws_vpc_endpoint" "servicecatalog" {
 resource "aws_vpc_endpoint" "lambda" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.lambda[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.lambda[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.lambda[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.lambda[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -312,10 +328,10 @@ resource "aws_vpc_endpoint" "lambda" {
 resource "aws_vpc_endpoint" "kms" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.kms[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.kms[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.kms[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.kms[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -326,10 +342,10 @@ resource "aws_vpc_endpoint" "kms" {
 resource "aws_vpc_endpoint" "logs" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.logs[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.logs[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.logs[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.logs[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -340,10 +356,10 @@ resource "aws_vpc_endpoint" "logs" {
 resource "aws_vpc_endpoint" "events" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.events[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.events[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.events[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.events[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -354,10 +370,10 @@ resource "aws_vpc_endpoint" "events" {
 resource "aws_vpc_endpoint" "states" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.states[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.states[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.states[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.states[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -368,10 +384,10 @@ resource "aws_vpc_endpoint" "states" {
 resource "aws_vpc_endpoint" "ssm" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.ssm[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.ssm[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.ssm[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.ssm[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -382,10 +398,10 @@ resource "aws_vpc_endpoint" "ssm" {
 resource "aws_vpc_endpoint" "sns" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.sns[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.sns[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.sns[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.sns[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -396,10 +412,10 @@ resource "aws_vpc_endpoint" "sns" {
 resource "aws_vpc_endpoint" "sqs" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.sqs[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.sqs[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.sqs[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.sqs[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
@@ -410,10 +426,10 @@ resource "aws_vpc_endpoint" "sqs" {
 resource "aws_vpc_endpoint" "sts" {
   count = var.aft_vpc_endpoints ? 1 : 0
 
-  vpc_id            = aws_vpc.aft_vpc.id
-  service_name      = data.aws_vpc_endpoint_service.sts[0].service_name
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = data.aws_subnets.sts[0].ids
+  vpc_id             = aws_vpc.aft_vpc.id
+  service_name       = data.aws_vpc_endpoint_service.sts[0].service_name
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = data.aws_subnets.sts[0].ids
   security_group_ids = [
     aws_security_group.aft_vpc_endpoint_sg.id,
   ]
